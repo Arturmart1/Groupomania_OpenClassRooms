b62d32aa8c71a9af5e487389cb43bd81060052e9
Ludovic T <thuard.ludovic@gmail.com>
2022-01-04 15:27:17 +0100

    Create user schema with sequelize

--- backend/models/userShema.js
+++ backend/models/userShema.js
@@ -1,7 +1,42 @@
-const userShema = {
-    name: { type: String, required: true},
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-};
+//Schema utilisateur
+const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
-module.exports = userShema;
+
+const user = Sequelize.define('User', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    freezeTableName: true,
+    instanceMethods: {
+        //Hash du mot de passe
+        generateHash(password) {
+            return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+        },
+        //Validation du mot de passe
+        validPassword(password) {
+            return bcrypt.compareSync(password, this.password);
+        },
+    },
+});
+
+return user;
