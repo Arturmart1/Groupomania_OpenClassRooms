524f6e1e23a3bf639bade2e9b152e646104df5fe
Ludovic T <thuard.ludovic@gmail.com>
2022-01-18 15:10:51 +0100

    Remove console log

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,7 +17,6 @@ passwordSchema
 //Création d'un utilisateur
 exports.signup = (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
-    console.log(req.body)
     if (!firstName || !lastName || !email || !password) {
         return res.status(400).json({ error: 'Veuillez remplir tous les champs' });
     }
@@ -58,33 +57,29 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
     const cryptedEmail = AES.encrypt(req.body.email);
-    User.findOne({
-        Where: {
-            email: cryptedEmail
+    User.findOne({ email: cryptedEmail })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
-    })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    res.status(200).json({
-                        userId: user.id,
-                        token: jwt.sign(
-                            { userId: user.id },
-                            process.env.TOKEN,
-                            { expiresIn: '24h' }
-                        ),
-                    });
-                }
-                )
-                .catch(error => res.status(500).json({ error, message: error.message }));
-        })
-};
+            res.status(200).json({
+              userId: user.id,
+              token: jwt.sign(
+                { userId: user.id }, 
+                '${process.env.TOKEN}',
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error, message: error.message }));
+      })
+      .catch(error => res.status(500).json({ error, message: error.message }));
+  };
 
 //Modification de l'utilisateur
 
