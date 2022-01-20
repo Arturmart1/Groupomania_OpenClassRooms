7afed3a26d641db6e86ea646deb224c148abefde
Ludovic T <thuard.ludovic@gmail.com>
2022-01-20 09:40:06 +0100

    Delete User controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -106,18 +106,17 @@ exports.modifyUser = (req, res, next) => {
 
 //Suppression d'un utilisateur par l'utilisateur ou un admin
 
-exports.deleteUser = (req, res, next) => {
-    const userId = req.userId;
-    User.findOne({ _id: userId, userId: req.token.userId })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            user.delete()
+exports.deleteUser = (req, res, next) => {;
+    User.findOne ({
+        where: {
+            id: req.params.id
+        }})
+        .then((user) => {
+            User.destroy({ where: { id: req.params.id } })
                 .then(() => res.status(200).json({ message: 'Utilisateur supprimé !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error, message: error.message }));
+                .catch(error => res.status(400).json({ error, message : error.message }));
+    })
+    .catch(error => res.status(500).json({ error, message: error.message }));
 };
 
 exports.gets = async () => {
