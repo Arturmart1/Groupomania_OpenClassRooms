50c0bf7c312afb72cdca784b248a0ce5d5bc023b
Ludovic T <thuard.ludovic@gmail.com>
2022-01-24 15:44:10 +0100

    Initialise Vue.js

---
+++ frontend/groupomania/src/components/homepage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <img src="../assets/media/icon-above-front.png" alt="logo groupomania" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        msg: String
+    }
+};
+</script>
+
+<style>
+</style>
