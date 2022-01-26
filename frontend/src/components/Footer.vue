0c0ea436e509c9b58b2f19fb7ebd960bc3be4c9e
Ludovic T <thuard.ludovic@gmail.com>
2022-01-26 15:16:18 +0100

    Header & footer components

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer class="footer--menu">
+        <nav v-if="userConnected">MON COMPTE</nav>
+        <nav v-if="userConnected">DECONNEXION</nav>
+    </footer>
+</template>
+
+<script>
+export default {
+    name : 'Footer',
+}
+</script>
+
+<style scoped lang="scss">
+    .footer--menu{
+        margin-top: 1rem;
+        position: sticky;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        nav{
+            background: red;
+            color: white;
+            font-weight: 600;
+            padding: 0.8rem;
+            border-radius: 0.8rem;
+        }
+    }
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header--container">
+        <div>
+            <img src="../assets/icon-left-font.png" alt="Logo Groupomania" class="header__logo">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name : 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+    .header--container{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        .header__logo{
+            width: 10rem;
+        }
+    }
+</style>
