58d45ab2a0f3bce5d130ad19237714f024e3d43d
Ludovic T <thuard.ludovic@gmail.com>
2022-02-08 10:15:41 +0100

    Table association

--- backend/models/initdb.js
+++ backend/models/initdb.js
@@ -8,14 +8,16 @@ const User = require('./userSchema');
 const Post = require('./postSchema');
 const Comment = require('./commentSchema');
 
-let Init = async () => {
+const Init = async () => {
     
     await User.sync({/*alter: true*/});
     await Post.sync({/*alter: true*/});
     await Comment.sync({/*alter: true*/});
+    User.belongsTo(Post, {foreignKey: 'postId'}, {onDelete:'CASCADE'});
+    Post.belongsTo(User, {foreignKey: 'userId'}, {onDelete:'CASCADE'});
+    Post.hasMany(Comment, {foreignKey: 'postId'}, {onDelete:'CASCADE'});
+    Comment.belongsTo(User, {foreignKey: 'userId'});
+    Comment.belongsTo(Post, {foreignKey: 'postId'});
 }
-User.belongsTo(Post, {foreignKey: 'post_id'}, {onDelete:'CASCADE'});
-Post.hasMany(Comment, {foreignKey: 'post_id'}, {onDelete:'CASCADE'});
-Comment.belongsTo(User, {foreignKey: 'user_id'});
 
 module.exports = Init;
