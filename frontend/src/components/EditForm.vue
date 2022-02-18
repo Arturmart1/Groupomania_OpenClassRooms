3969580957b2e9734730702bec4ce5b733a7441a
Ludovic T <thuard.ludovic@gmail.com>
2022-02-18 13:55:22 +0100

    Add style

--- frontend/src/components/EditForm.vue
+++ frontend/src/components/EditForm.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="form--card">
         <div class="post--form">
-            <h2>Modification</h2>
-            <form name="editPost" id="editPost">
-                <input type="text" placeholder="{{title}}" required v-model="postInput.title">
-                <input type="text" placeholder="{{content}}" required v-model="postInput.content">
-                <input type="file" placeholder="Importez votre image" ref="imageUrl" name="image" id="imageUrl" accept="image/*">
-            </form>
+            <div>
+                <h2>Modification</h2>
+            </div>
+            <div>
+                <form name="editPost" id="editPost">
+                    <input type="text" placeholder="{{title}}" required v-model="postInput.title">
+                    <input type="text" placeholder="{{content}}" required v-model="postInput.content">
+                    <input type="file" placeholder="Importez votre image" ref="imageUrl" name="image" id="imageUrl" accept="image/*">
+                </form>
+            </div>
         </div>
         <div class="command__center">
             <div class="command__button" @click.prevent="updatePost(postInput.postId)">
@@ -97,38 +101,67 @@ export default {
 
 <style lang="scss" scoped>
 .form--card{
-    width: 100%;
-    height: 100%;
+    width: 70%;
+    height: 25vh;
+    margin: auto;
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    border-radius: 1rem;
+    padding: 1rem;
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+    form{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        margin-top: 1rem;
+        input{
+            margin: 0.5rem;
+            border: unset;
+            border-bottom: #FF4B2B 1px solid;
+            &:focus{
+                outline: none;
+            }
+        }
+    }
+    h2{
+        font-size: 1.5em;
+        font-weight: 600;
+    }
+    .command__center{
+        width: 80%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        .command__button{
+            background: #FF4B2B;
+            color: white;
+            font-size: 1.3em;
+            font-weight: 500;
+            padding: 1rem;
+            width: 5rem;
+            border-radius: 1rem;
+            &:hover{
+                cursor: pointer;
+                animation: jelly 0.5s
+            }
+        }
+    }
 }
-.post--form{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-.command__center{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-}
-.command__button{
-    width: 100px;
-    height: 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    background-color: #FF4B2B;
-    color: white;
+@keyframes jelly {
+  0%,
+  100% {
+    transform: scale(1, 1);
+  }
+  25% {
+    transform: scale(0.9, 1.1);
+  }
+  50% {
+    transform: scale(1.1, 0.9);
+  }
+  75% {
+    transform: scale(0.95, 1.05);
+  }
 }
 </style>
