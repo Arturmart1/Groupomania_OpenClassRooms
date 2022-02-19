df1611ad08558be331430e73c8d426b0023e7dc0
Ludovic <thuard.ludovic@gmail.com>
2022-02-19 14:37:23 +0100

    Relation's corrections

--- backend/models/initdb.js
+++ backend/models/initdb.js
@@ -5,9 +5,11 @@ const Post = require('./postSchema');
 const Comment = require('./commentSchema');
 
 const Init = async () => {
+    User.hasMany(Post, {onDelete:'CASCADE'});
     Post.belongsTo(User, {onDelete: 'CASCADE'});
-    Post.hasMany(Comment, {onDelete: 'CASCADE'});
+    User.hasMany(Comment, {onDelete: 'CASCADE'});
     Comment.belongsTo(User);
+    Post.hasMany(Comment, {onDelete: 'CASCADE'});
     await User.sync({/*alter: true*/});
     await Post.sync({/*alter: true*/});
     await Comment.sync({/*alter: true*/});
